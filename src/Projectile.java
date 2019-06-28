feadf02f8678be877e6e6493c3d678d1719d033d
Aeytrg1248 <andryusha.zarubin.90@gmail.com>
2019-06-29 01:59:51 +0300

    movement of projectle
    kek

--- src/Projectile.java
+++ src/Projectile.java
@@ -4,5 +4,46 @@ public class Projectile extends Entity {
 			super(ID, HP, Radius, MS, DMG, x, y, "bullet.png");
 			// TODO Auto-generated constructor stub
 		}
+		public void MoveRight(int x, int y)
+		{
+			x=+3;
+			y=y*1;
+		}
+		public void MoveLeft(int x, int y)
+		{
+			x=-3;
+			y=y*1;
+		}
+		public void MoveUp(int x int y)
+		{
+			x=x*1;
+			y=+3;
+		}
+		public void MoveDown(int x, int y)
+		{
+			x=x*1;
+			y=-3;
+		}
+		public void MoveRightUp(int x, int y)
+		{
+			x=+3;
+			y=+3;
+		}
+		public void MoveRightDown(int x, int y)
+		{
+			x=+3;
+			y=-3;
+		}
+		public void MoveLeftUp(int x, int y)
+		{
+			x=-3;
+			y=+3;
+		}
+		public void MoveLeftDown(int x, int y)
+		{
+			x=-3;
+			y=-3;
+		}
+		
 
 }
